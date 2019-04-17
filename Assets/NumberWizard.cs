4f8160bf443d8da4c6b1491ce7c3e9c677b78cf4
Ayaan-Axb <49731441+Ayaan-Axb@users.noreply.github.com>
2019-04-17 21:27:31 +0100

    Alright
    123

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -20,7 +20,7 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than 500");
-        Debug.Log("Push Up = higher, Push Down = Lower, Push Enter = Correct.............");
+        Debug.Log("Push Up = higher, Push Down = Lower, Push Enter = Correct.................");
     }
 
     // Update is called once per frame
